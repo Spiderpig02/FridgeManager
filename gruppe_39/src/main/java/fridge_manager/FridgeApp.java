1dd15fbf15c987be42e6026b64df56cc32097979
Halvor Øverby <halvorov@stud.ntnu.no>
2022-09-15 12:12:39 +0200

    add basic app structure

--- gruppe_39/src/main/java/fridge_manager/FridgeApp.java
+++ gruppe_39/src/main/java/fridge_manager/FridgeApp.java
@@ -1,13 +1,28 @@
 package fridge_manager;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
- * Hello world!
- *
+ * JavaFX App
  */
-public class FridgeApp 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class FridgeApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("FridgeApp.fxml"));
+        Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
+

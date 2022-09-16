7a14142b28060a9fb38013a82b04d56d47fa66df
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2022-09-16 11:03:37 +0200

    Completed basic FXML and link between FXML and Controller

--- gruppe_39/src/main/java/fridge_manager/FridgeController.java
+++ gruppe_39/src/main/java/fridge_manager/FridgeController.java
@@ -1,5 +1,24 @@
 package fridge_manager;
 
+//FXML-imports
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+
+
 public class FridgeController {
     
+    @FXML private TextField textfield_matvare;
+    @FXML private TextField textfield_antall;
+    @FXML private TextField textfield_utlopsdato;
+    @FXML private TextField textfield_eier;
+    @FXML private Button legg_til_button;
+    @FXML private ListView fridgecontent;
+
+    @FXML
+    private void addToFridge() {
+        
+    }    
+
 }

b7220f4667854aa2382d1e4166676397736f608a
Daniel N Hansen <danielnh@stud.ntnu.no>
2022-09-21 16:39:27 +0200

    Changed the POM to maybe work

--- gruppe_39/src/test/java/fridge_manager/FileHandlerTest.java
+++ gruppe_39/src/test/java/fridge_manager/FileHandlerTest.java
@@ -1,14 +1,21 @@
 package fridge_manager;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class FileHandlerTest {
 
-    FridgeManager fridge;
+    static FridgeManager fridge;
 
     @BeforeAll
-    public void startUp() {
-        fridge = new FridgeManager();
-        fridge.Frigde(50, 50);
+    public static void startUp() {
+        fridge = new FridgeManager(50, 50);
+    }
+
+    @Test
+    public void testFileSavingAndRetriving() {
+        fridge.addFreezerContent(new Food("Pizza", 1, "Mr.meseeks", "2022.12.12"));
+        fridge.addFridgeContent(new Food("Pizza", 1, "Mr.meseeks", "2022.12.12"));
+
     }
 }

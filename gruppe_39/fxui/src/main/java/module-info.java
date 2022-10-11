ec0f41248573d794fd6bb875fea3efb518dcc10f
Halvor Øverby <halvorov@stud.ntnu.no>
2022-10-11 18:03:39 +0200

    fix: add more requirements to module info

--- gruppe_39/fxui/src/main/java/module-info.java
+++ gruppe_39/fxui/src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module fridge_manager.fxui {
-    requires com.fasterxml.jackson.databind;
-
-    requires javafx.base;
+    requires fridge_manager.core;
     requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
     requires javafx.fxml;
 
-    requires fridge_manager.core;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
     opens fridge_manager.fxui to javafx.graphics, javafx.fxml;
+    exports fridge_manager.fxui;
 }

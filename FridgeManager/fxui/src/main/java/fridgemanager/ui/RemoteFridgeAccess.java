a3fe6b9c80fcb5d925ff549ccf104d813494775e
Halvor Øverby <halvorov@stud.ntnu.no>
2022-11-04 11:43:56 +0100

    feat: add constructor for remoteFridgeAccess
    add constructor and basic variables and methods
    
    @jmberget
    @halvorov
    (36)

--- FridgeManager/fxui/src/main/java/fridgemanager/ui/RemoteFridgeAccess.java
+++ FridgeManager/fxui/src/main/java/fridgemanager/ui/RemoteFridgeAccess.java
@@ -1,5 +1,84 @@
 package fridgemanager.ui;
 
-public class RemoteFridgeAccess {
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fridgemanager.core.FridgeManager;
+
+
+
+public class RemoteFridgeAccess {
+    private URI endpointBaseUri;
+    
+    private static final String APPLICATION_JSON = "springboot/fridgemanager/getFridgeContent";
+
+    private static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+  
+    private static final String ACCEPT_HEADER = "Accept";
+  
+    private static final String CONTENT_TYPE_HEADER = "Content-Type";
+
+    private ObjectMapper objectMapper;
+  
+    private FridgeManager fridgeManager;
+
+  public RemoteFridgeAccess(URI endpointBaseUri) {
+    this.endpointBaseUri = endpointBaseUri;
+  }
+
+
+  public FridgeManager getFridgeManager() {
+    if (fridgeManager == null) {
+      HttpRequest request = HttpRequest.newBuilder(endpointBaseUri)
+          .header(ACCEPT_HEADER, APPLICATION_JSON)
+          .GET()
+          .build();
+      try {
+        final HttpResponse<String> response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+        this.fridgeManager = objectMapper.readValue(response.body(), FridgeManager.class);
+      } catch (IOException | InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    System.out.println(fridgeManager.toString());
+    return fridgeManager;
+  }
+
+  public static void main(String[] args) throws URISyntaxException {
+    RemoteFridgeAccess access = new RemoteFridgeAccess(new URI("localhost:8080"));
+    access.getFridgeManager();
+  }
+
+
+
+
+
+//   private void putTodoList(AbstractTodoList todoList) {
+//     try {
+//       String json = objectMapper.writeValueAsString(todoList);
+//       HttpRequest request = HttpRequest.newBuilder(todoListUri(todoList.getName()))
+//           .header(ACCEPT_HEADER, APPLICATION_JSON)
+//           .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
+//           .PUT(BodyPublishers.ofString(json))
+//           .build();
+//       final HttpResponse<String> response =
+//           HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+//       String responseString = response.body();
+//       Boolean added = objectMapper.readValue(responseString, Boolean.class);
+//       if (added != null) {
+//         todoModel.putTodoList(todoList);
+//       }
+//     } catch (IOException | InterruptedException e) {
+//       throw new RuntimeException(e);
+//     }
+//  }
 }

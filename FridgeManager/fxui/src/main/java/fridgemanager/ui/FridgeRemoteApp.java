6311d380139e50af3c51a16d29d8681f7034dbaf
Halvor Øverby <halvorov@stud.ntnu.no>
2022-11-10 14:01:58 +0100

    add basic fridgeremote app
    add basic code for fridgeremoteapp.fxml and fridgeremoteapp.java
    
    (#36)
    
    @halvorov
    @DanielNHansen

---
+++ FridgeManager/fxui/src/main/java/fridgemanager/ui/FridgeRemoteApp.java
@@ -0,0 +1,22 @@
+package fridgemanager.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FridgeRemoteApp extends Application{
+ 
+    @Override
+    public void start(Stage stage) throws Exception {
+      Parent parent = FXMLLoader.load(getClass().getResource("FridgeRemoteApp.fxml"));
+      stage.setScene(new Scene(parent));
+      stage.show();
+    }
+  
+    public static void main(String[] args) {
+      launch(FridgeRemoteApp.class, args);
+    }
+     
+}
